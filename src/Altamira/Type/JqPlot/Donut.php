81d35ce6b8b41eba36ebe1b3dffc38a75246a630
Robert Elwell <robert@wikia-inc.com>
2012-08-04 12:44:26 -0700

    update jqplot donut type to handle option setting better

--- src/Altamira/Type/JqPlot/Donut.php
+++ src/Altamira/Type/JqPlot/Donut.php
@@ -4,6 +4,18 @@ namespace Altamira\Type\JqPlot;
 
 class Donut extends Pie
 {
+    protected $options = array('seriesDefaults'=>array('rendererOptions'=>array()));
+    
+    public function getOptions()
+    {
+        return $this->options;
+    }
+    
+    public function setOption($name, $value)
+    {
+        $this->options['seriesDefaults']['rendererOptions'][$name] = $value;
+        return $this; 
+    }
 }
 
 ?>
