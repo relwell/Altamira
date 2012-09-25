52e4c47d6affc7b0525a848903e0e442d80379b7
Jonathan Chan <jc@jmccc.com>
2012-09-25 18:59:24 +0000

    added stub test

---
+++ Tests/DefaultTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Malwarebytes\AltamiraBundle\Tests;
+
+class DefaultTest extends \PHPUnit_Framework_TestCase {
+    public function testDefault() {
+        $this->fail("Tests are unimplemented! Please implement proper tests!");
+    }
+}
+?>
