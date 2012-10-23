4a5d5046f262ada850efa4bc36d4d4a71fef05ea
Josh Hall-Bachner <jhallbachner@gmail.com>
2012-10-23 11:16:27 -0700

    Updated a variety of classes to remove notices originating from unset variables.

--- src/Altamira/ChartRenderer.php
+++ src/Altamira/ChartRenderer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Altamira;
 use Altamira\ChartRenderer\RendererInterface;
@@ -8,55 +8,49 @@ use Altamira\ChartRenderer;
 class ChartRenderer
 {
     protected static $rendererChain = array();
-    
+
     public static function render( Chart $chart, array $styleOptions = array() )
     {
         if ( empty(self::$rendererChain) ) {
             self::pushRenderer( '\Altamira\ChartRenderer\DefaultRenderer' );
-        } 
-            
+        }
+
         $outputString = '';
 
         for ( $i = count(self::$rendererChain)-1; $i >= 0; $i-- ) {
             $renderer = self::$rendererChain[$i];
             $outputString .= call_user_func_array(array($renderer, 'preRender'), array( $chart, $styleOptions ));
         }
-        
+
         for ( $i = 0; $i < count(self::$rendererChain); $i++ ) {
             $renderer = self::$rendererChain[$i];
             $outputString .= call_user_func_array(array($renderer, 'postRender'), array( $chart, $styleOptions ));
         }
-        
+
         return $outputString;
     }
-    
+
     public static function pushRenderer( $renderer )
     {
         if (! in_array( 'Altamira\ChartRenderer\RendererInterface', class_implements( $renderer ) ) ) {
             throw new \UnexpectedValueException( "Renderer must be instance of or string name of a class implementing RendererInterface" );
         }
-        
+
         array_push( self::$rendererChain, $renderer );
-        
-        return self;
     }
-    
+
     public static function unshiftRenderer( $renderer )
     {
         if (! in_array( 'Altamira\ChartRenderer\RendererInterface', class_implements( $renderer ) ) ) {
             throw new \UnexpectedValueException( "Renderer must be instance of or string name of a class implementing RendererInterface" );
         }
-        
+
         array_unshift( self::$rendererChain, $renderer );
-        
-        return self;
     }
-    
+
     public function reset()
     {
         self::$rendererChain = array();
-        
-        return self;
     }
-    
+
 }
--- src/Altamira/JsWriter/JsWriterAbstract.php
+++ src/Altamira/JsWriter/JsWriterAbstract.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
 namespace Altamira\JsWriter;
 
 abstract class JsWriterAbstract
 {
     protected $chart;
-    
+
     protected $options = array('series'=>array());
     protected $files = array();
     protected $callbacks = array();
@@ -14,55 +14,55 @@ abstract class JsWriterAbstract
     protected $types = array();
     protected $library;
     protected $typeNamespace;
-    
+
     public function __construct(\Altamira\Chart $chart)
     {
         $this->chart = $chart;
     }
-    
+
     public function getScript()
     {
         $options = array_merge($this->chart->getOptions(), $this->options);
         $this->options = $this->getTypeOptions($this->getSeriesOptions($options));
-        
+
         return $this->generateScript();
     }
-    
+
     public function makeJSArray($array)
     {
         $options = json_encode($array);
         $optionString = preg_replace('/"#(.*?)#"/', '$1', $options);
-        
+
         foreach ( $this->callbacks as $placeHolder => $callback ) {
             $optionString = str_replace("\"{$placeHolder}\"", $callback, $optionString);
         }
-        
+
         return $optionString;
     }
-    
+
     protected function getCallbackPlaceholder( $callback )
     {
         $index = count($this->callbacks);
         $uid = spl_object_hash( $this );
-        
+
         $key = sprintf('%s_%s', $uid, $index);
-        
+
         $this->callbacks[$key] = $callback;
-        
-        return $key; 
+
+        return $key;
     }
-    
+
     public function getFiles()
     {
         $files = $this->files;
-        
+
         foreach ($this->types as $type) {
             $files = array_merge($files, $type->getFiles());
         }
-        
+
         return $files;
     }
-    
+
     public function getOptionsForSeries($series)
     {
         if ($series instanceOf \Altamira\Series) {
@@ -71,29 +71,31 @@ abstract class JsWriterAbstract
             return $this->options['series'][$series];
         }
     }
-    
+
     public function getSeriesOption($series, $option)
     {
-        if ($series instanceOf \Altamira\Series) {
+        if ($series instanceOf \Altamira\Series && isset($this->options['series'][$series->getTitle()][$option])) {
             return $this->options['series'][$series->getTitle()][$option];
-        } else if (is_string($series)) {
+        } else if (is_string($series) && isset($this->options['series'][$series][$option])) {
             return $this->options['series'][$series][$option];
+        } else {
+            return null;
         }
     }
-    
+
     public function setSeriesOption( \Altamira\Series  $series, $name, $value)
     {
         $this->options['series'][$series->getTitle()][$name] = $value;
-        
+
         return $this;
     }
-    
+
     public function initializeSeries( \Altamira\Series $series )
     {
         $this->options['seriesStorage'][$series->getTitle()] = array();
         $this->series[] = $series;
     }
-    
+
     public function getLibrary()
     {
         if (!$this->library) {
@@ -101,38 +103,38 @@ abstract class JsWriterAbstract
         }
         return $this->library;
     }
-    
+
     public function setType( $type, $series = null )
     {
         if ( $series instanceOf \Altamira\Series ) {
             $series = $series->getTitle();
         }
-        
+
         $title = $series ?: 'default';
-    
+
         $className =  $this->typeNamespace . ucwords($type);
         if(class_exists($className)) {
             $this->types[$title] = new $className($this);
         }
-        
+
         return $this;
     }
-    
+
     public function setTypeOption( $name, $option, $series=null )
     {
         if ( $series instanceOf \Altamira\Series ) {
             $series = $series->getTitle();
         }
-        
+
         $title = $series ?: 'default';
-        
+
         if(isset($this->types[$title])) {
             $this->types[$title]->setOption($name, $option);
         }
-        
+
         return $this;
     }
-    
+
     abstract protected function getSeriesOptions(array $options);
     abstract protected function getTypeOptions(array $options);
     abstract protected function generateScript();
