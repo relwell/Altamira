9c35b8423981a1470c62ace968d444c88a30a8ca
Robert Elwell <robert@wikia-inc.com>
2012-10-25 18:50:07 -0700

    Create PHPUnit architecture, stub out SeriesTest to make sure everything works

---
+++ test/SeriesTest.php
@@ -0,0 +1,15 @@
+<?
+
+use Altamira\Series;
+
+class SeriesTest extends PHPUnit_Framework_TestCase
+{
+    
+    
+    public function testFoo()
+    {
+        $this->assertTrue( true );
+    }
+    
+    
+}
