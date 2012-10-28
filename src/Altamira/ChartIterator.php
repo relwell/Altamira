5f479cf9412bc847e6183eb9740c24fc04f4e939
Robert Elwell <robert@wikia-inc.com>
2012-10-28 14:25:37 -0700

    begin writing chart test -- got full code coverage for chartiterator

--- src/Altamira/ChartIterator.php
+++ src/Altamira/ChartIterator.php
@@ -4,12 +4,34 @@ namespace Altamira;
 
 class ChartIterator extends \ArrayIterator
 {
-    
+    /**
+     * Stores the plugins in an iterator that knows how to render them
+     * @var \Altamira\FilesRenderer
+     */
     protected $plugins;
+    /**
+     * Stores inline script calls in an iterator that knows how to render them
+     * @var \Altamira\ScriptsRenderer
+     */
     protected $scripts;
+    /**
+     * Stores global configurations as set in altamira-config.ini
+     * @var \Altamira\Config
+     */
     protected $config;
+    /**
+     * The key is the library name; the value is immaterial. This is done to ensure uniqueness.
+     * @var array
+     */
+    protected $libraries;
     
-    public function __construct( $array, $config )
+    /**
+     * Constructor method
+     * @param array $charts array of \Altamira\Chart instances
+     * @param \Altamira\Config $config
+     * @throws \Exception
+     */
+    public function __construct( array $charts, \Altamira\Config $config )
     {
         $this->config = $config;
         
@@ -17,23 +39,22 @@ class ChartIterator extends \ArrayIterator
         $plugins = array();
         $scripts = array();
         
-        foreach ($array as $item) {
-            if (! $item instanceOf Chart ) {
-                throw new \Exception("ChartIterator only supports an array of Chart instances.");
+        foreach ($charts as $chart) {
+            if (! $chart instanceof Chart ) {
+                throw new \UnexpectedValueException("ChartIterator only supports an array of Chart instances.");
             }
             
             // time saver -- if it's a chart, we can use this loop to add files, too
-            $plugins = array_merge($plugins, $item->getFiles());
-            $scripts[] = $item->getScript();
-            $this->libraries[$item->getLibrary()] = true;
+            $plugins = array_merge( $plugins, $chart->getFiles() );
+            $scripts[] = $chart->getScript();
+            $this->libraries[$chart->getLibrary()] = true;
         }
 
-        // yo dawg...
-        $this->plugins = new FilesRenderer($plugins, $config['js.pluginpath']);
-        $this->scripts = new ScriptsRenderer($scripts);
+        $this->plugins = new FilesRenderer( $plugins, $config['js.pluginpath'] );
+        $this->scripts = new ScriptsRenderer( $scripts );
         
         
-        parent::__construct($array);        
+        parent::__construct( $charts );        
     }
     
     
@@ -42,7 +63,10 @@ class ChartIterator extends \ArrayIterator
      * We don't handle chart HTML this way since placement and context is a front-end concern.  
      */
     
-    
+    /**
+     * Echoes out script tags referencing JS files
+     * @return \Altamira\ChartIterator provides fluent interface
+     */
     public function renderPlugins()
     {
         
@@ -57,6 +81,10 @@ class ChartIterator extends \ArrayIterator
         
     }
     
+    /**
+     * Echoes out inline javascript. Note that we group it all together in a single script tag.
+     * @return \Altamira\ChartIterator provides fluent interface
+     */
     public function renderScripts()
     {
         echo "<script type='text/javascript'>\n";
@@ -72,6 +100,10 @@ class ChartIterator extends \ArrayIterator
         
     }
     
+    /**
+     * Provides libraries paths based on config values
+     * @return array
+     */
     public function getLibraries()
     {
         $libraryToPath = array(
@@ -88,19 +120,27 @@ class ChartIterator extends \ArrayIterator
         return $libraryPaths;
     }
     
+    /**
+     * Echoes library plugins for invoking in the DOM head
+     * @return \Altamira\ChartIterator provides fluent interface
+     */
     public function renderLibraries()
     {
-        foreach ($this->getLibraries() as $libraryPath) {
+        foreach ( $this->getLibraries() as $libraryPath ) {
             echo "<script type='text/javascript' src='$libraryPath'></script>";
         }
         
         return $this;
     }
     
+    /**
+     * Echoes any CSS that is needed for the libraries to render correctly
+     * @return \Altamira\ChartIterator provides fluent interface
+     */
     public function renderCss()
     {
-        foreach ($this->libraries as $library=>$junk) {
-            switch($library) {
+        foreach ( $this->libraries as $library => $junk ) {
+            switch( $library ) {
                 case 'flot':
                     break;
                 case 'jqPlot':
@@ -110,7 +150,7 @@ class ChartIterator extends \ArrayIterator
         
         }
         
-        if (isset($cssPath)) {
+        if ( isset( $cssPath ) ) {
             echo "<link rel='stylesheet' type='text/css' href='{$cssPath}'></link>";
         }
         
---
+++ test/ChartTest.php
@@ -0,0 +1,148 @@
+<?php
+
+class ChartTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @covers \Altamira\ChartIterator::__construct
+     * @covers \Altamira\ChartIterator::getLibraries
+     * @covers \Altamira\ChartIterator::renderCss
+     * @covers \Altamira\ChartIterator::renderLibraries
+     * @covers \Altamira\ChartIterator::renderPlugins
+     * @covers \Altamira\ChartIterator::renderScripts
+     */
+    public function testChartIterator()
+    {
+        $mockConfig = $this->getMock( '\Altamira\Config', array( 'offsetGet' ), array( '../altamira-config.ini' ) );
+        
+        $junkCharts = array( 'chart1', 'chart2' );
+        
+        $exception = null;
+        try {
+            $ci = new \Altamira\ChartIterator( $junkCharts, $mockConfig );
+        } catch ( Exception $e ) {
+            $exception = $e;
+        }
+        
+        $this->assertInstanceOf(
+                'UnexpectedValueException',
+                $exception,
+                '\Altamira\ChartIterator::__construct should test that the array passed to it contains only instances of \Altamira\Chart'
+        );
+        
+        $mockChart1 = $this->getMock( '\Altamira\Chart', array( 'getFiles', 'getScript', 'getLibrary' ) );
+        $mockChart2 = $this->getMock( '\Altamira\Chart', array( 'getFiles', 'getScript', 'getLibrary' ) );
+        
+        $mockChart1
+            ->expects( $this->once() )
+            ->method ( 'getFiles' )
+            ->will   ( $this->returnValue( array( 'file1a.js', 'file1b.js' ) ) )
+        ;
+        $mockChart2
+            ->expects( $this->once() )
+            ->method ( 'getFiles' )
+            ->will   ( $this->returnValue( array( 'file2a.js', 'file2b.js' ) ) )
+        ;
+        $mockChart1
+            ->expects( $this->once() )
+            ->method ( 'getScript' )
+            ->will   ( $this->returnValue( '(function(alert("hey");))();' ) );
+        ;
+        $mockChart2
+            ->expects( $this->once() )
+            ->method ( 'getScript' )
+            ->will   ( $this->returnValue( '(function(alert("ho");))();' ) );
+        ;
+        $mockChart1
+            ->expects( $this->once() )
+            ->method ( 'getLibrary' )
+            ->will   ( $this->returnValue( 'flot' ) )
+        ;
+        $mockChart2
+            ->expects( $this->once() )
+            ->method ( 'getLibrary' )
+            ->will   ( $this->returnValue( 'jqPlot' ) )
+        ;
+        $cssPath = 'css/jqplot.css';
+        $mockConfig
+            ->expects( $this->at( 0 ) )
+            ->method ( 'offsetGet' )
+            ->with   ( 'js.pluginpath' )
+            ->will   ( $this->returnValue( 'js/' ) )
+        ;
+        $mockConfig
+            ->expects( $this->at( 1 ) )
+            ->method ( 'offsetGet' )
+            ->with   ( 'css.jqplotpath' )
+            ->will   ( $this->returnValue( $cssPath ) )
+        ;
+        $mockConfig
+            ->expects( $this->at( 2 ) )
+            ->method ( 'offsetGet' )
+            ->with   ( 'js.flotpath' )
+            ->will   ( $this->returnValue( 'flot.js' ) )
+        ;
+        $mockConfig
+            ->expects( $this->at( 3 ) )
+            ->method ( 'offsetGet' )
+            ->with   ( 'js.jqplotpath' )
+            ->will   ( $this->returnValue( 'jqplot.js' ) )
+        ;
+        
+        $mockCharts = array( $mockChart1, $mockChart2 );
+        
+        $chartIterator = new \Altamira\ChartIterator( $mockCharts, $mockConfig );
+        
+        $plugins   = new ReflectionProperty( '\Altamira\ChartIterator', 'plugins' );
+        $scripts   = new ReflectionProperty( '\Altamira\ChartIterator', 'scripts' );
+        $libraries = new ReflectionProperty( '\Altamira\ChartIterator', 'libraries' );
+        
+        $plugins->setAccessible( true );
+        $scripts->setAccessible( true );
+        $libraries->setAccessible( true );
+        
+        $this->assertInstanceOf(
+                '\Altamira\FilesRenderer',
+                $plugins->getValue( $chartIterator ),
+                '\Altamira\ChartIterator should create an instance of \Altamira\FilesRenderer during construction'
+        );
+        
+        $this->assertInstanceOf(
+                '\Altamira\ScriptsRenderer',
+                $scripts->getValue( $chartIterator ),
+                '\Altamira\ChartIterator should create an instance of \Altamira\ScriptsRenderer during construction'
+        );
+        
+        $this->assertEquals(
+                array( 'flot' => true, 'jqPlot' => true ),
+                $libraries->getValue( $chartIterator ),
+                '\Altamira\ChartIterator should unique-keyed hash table of all libraries used by all charts'
+        );
+        
+        $expectedOutputString = "<link rel='stylesheet' type='text/css' href='{$cssPath}'></link>";
+        $expectedOutputString .= "<script type='text/javascript' src='flot.js'></script>";
+        $expectedOutputString .= "<script type='text/javascript' src='jqplot.js'></script>";
+        
+        $expectedOutputString .= <<<ENDSTRING
+<script type="text/javascript" src="js/file1a.js"></script>
+<script type="text/javascript" src="js/file1b.js"></script>
+<script type="text/javascript" src="js/file2a.js"></script>
+<script type="text/javascript" src="js/file2b.js"></script>
+<script type='text/javascript'>
+(function(alert("hey");))();(function(alert("ho");))();
+</script>
+
+ENDSTRING;
+        
+        $this->expectOutputString(
+                $expectedOutputString,
+                '\Altamira\ChartIterator should render libraries, CSS, and plugins'
+        );
+        
+        $chartIterator->renderCss()
+                      ->renderLibraries()
+                      ->renderPlugins()
+                      ->renderScripts();
+        
+    }
+    
+}
